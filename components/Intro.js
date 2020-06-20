98b6304f2b9dfcec0b6b477d282158e3cf90b4f4
bajpai244 <bajpaiharsh244@gmail.com>
2020-06-20 16:14:57 +0530

    removed some effects

--- components/Intro.js
+++ components/Intro.js
@@ -3,6 +3,7 @@ import { jsx, Grid, Box } from 'theme-ui'
 import Fade from 'react-reveal/Fade'
 import Bounce from 'react-reveal/Bounce'
 import Zoom from 'react-reveal/Zoom'
+import Flip from 'react-reveal/Flip'
 import config from 'react-reveal/globals'
 config({ ssrFadeout: true })
 
@@ -27,21 +28,18 @@ export default () => (
       }
     }}
   >
-    <Zoom>
-      <p sx={{ variant: 'para', mt: 4 }}>Hack Club-India Amazon</p>
-      <p sx={{ variant: 'para', textAlign: 'center', mb: 4 }}>
-        Alexa Challenge
-      </p>
-    </Zoom>
-    <Bounce bottom>
-      {' '}
-      <img
-        src={'./moreecho2.png'}
-        sx={{ width: ['250px', 'initial'] }}
-        alt={'Alexa Image'}
-      />
-    </Bounce>
-
+    <p sx={{ variant: 'para', mt: 4 }}>
+      <span className="Orange" sx={{ color: '#FF8000' }}>
+        Hack
+      </span>
+      <span> Club</span>-<span sx={{ color: '#34D54A' }}>India</span> Amazon
+    </p>
+    <p sx={{ variant: 'para', textAlign: 'center', mb: 4 }}>Alexa Challenge</p>
+    <img
+      src={'./moreecho2.png'}
+      sx={{ width: ['250px', 'initial'] }}
+      alt={'Alexa Image'}
+    />
     <Bounce>
       <p sx={{ fontWeight: 'bold', mt: 0, fontSize: [1, 3, 4, 4] }}>
         Your chance to win an Amazon Alexa Echo Dot{' '}
