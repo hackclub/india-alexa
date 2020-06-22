809c5cd1a609f746deaa58f565e14398e8c82dcf
bajpai244 <bajpaiharsh244@gmail.com>
2020-06-22 17:15:54 +0530

    added faq

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const withImages = require('next-images')
+const withMDX = require('@next/mdx')()
+module.exports = withMDX()
+
+/*const withImages = require('next-images')
 module.exports = withImages({
   webpack(config, options) {
     return config
   }
 })
+*/
